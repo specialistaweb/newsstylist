5662fd3837bac77972485d7c0f9f50832df7aeb8
Alessandro Tuveri <at@specialistaweb.it>
2022-02-22 20:55:35 +0100

    compatibility to bootstrap 12 and news 9.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "newsstylist".
  *
- * Auto generated 22-12-2020 09:21
+ * Auto generated 11-02-2022 08:40
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -14,19 +14,20 @@ $EM_CONF[$_EXTKEY] = array (
   'title' => 'News StyList',
   'description' => 'all in one: one-touch settings within news plugin - fast & cool way to change the output of news, List view 5 layouts (default, card, carousel, newsletter) and swiper slider with some effects',
   'category' => 'plugin',
-  'author' => 'Alessandro Tuveri (University of Udine), Giulia Tuveri (specialistaweb.it)',
-  'author_email' => 'at@uniud.it, gt@specialistaweb.it',
+  'version' => '1.8.0',
   'state' => 'beta',
   'uploadfolder' => false,
-  'clearCacheOnLoad' => 0,
-  'version' => '1.8.0',
+  'clearcacheonload' => false,
+  'author' => 'Alessandro Tuveri (University of Udine), Giulia Tuveri (specialistaweb.it)',
+  'author_email' => 'at@uniud.it, gt@specialistaweb.it',
+  'author_company' => NULL,
   'constraints' => 
   array (
     'depends' => 
     array (
-      'typo3' => '8.7.0-10.99.99',
-      'bootstrap_package' => '9.0.0-11.99.99',
-      'news' => '7.3.0-8.99.99',
+      'typo3' => '10.0.0-11.5.99',
+      'bootstrap_package' => '11.0.3-12.0.99',
+      'news' => '9.0.0-9.99.99',
     ),
     'conflicts' => 
     array (
@@ -35,15 +36,5 @@ $EM_CONF[$_EXTKEY] = array (
     array (
     ),
   ),
-  'autoload' => 
-  array (
-    'psr-4' => 
-    array (
-      'SpecialistaWeb\\Newsstylist\\' => 'Classes',
-    ),
-  ),
-  'clearcacheonload' => false,
-  'author_company' => NULL,
-  'createDirs' => NULL,
 );
 
