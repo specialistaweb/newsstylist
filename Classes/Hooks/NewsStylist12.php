95c249fd496e597a45ccde63499f95314c4326af
Alessandro Tuveri <at@specialistaweb.it>
2023-11-03 17:54:33 +0100

    new compat version 4.0.1 / TYPO3 v12-BP 14
    flexform and configuration files to ensure compatibility with TYPO3 v,12.4.x and Bootstrap Package 14.0.x

---
+++ Classes/Hooks/NewsStylist12.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SpecialistaWeb\Newsstylist\Hooks;
+
+/**
+ * This file is part of the "eventnews" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
+use TYPO3\CMS\Core\Configuration\Event\AfterFlexFormDataStructureParsedEvent;
+use TYPO3\CMS\Core\Information\Typo3Version;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class NewsStylist12
+{
+
+    public function __invoke(AfterFlexFormDataStructureParsedEvent $event): void
+    {
+        $dataStructure = $event->getDataStructure();
+        $identifier = $event->getIdentifier();
+
+        if ($identifier['type'] === 'tca' && $identifier['tableName'] === 'tt_content' && $this->isActiveOnKey($identifier['dataStructureKey'])) {
+            $content = file_get_contents($this->getPath());
+            //ù$content .= file_get_contents($this->getPathSwiper());
+            if ($content) {
+                $dataStructure['sheets']['newsstylist'] = GeneralUtility::xml2array($content);
+            }
+        }
+
+        $content = file_get_contents($this->getPathSwiper());
+            if ($content) {
+                $dataStructure['sheets']['swiperslider'] = GeneralUtility::xml2array($content);
+	   }
+        $event->setDataStructure($dataStructure);
+  
+  }
+
+    public function parseDataStructureByIdentifierPostProcess(array $dataStructure, array $identifier): array
+    {
+        if ($identifier['type'] === 'tca' && $identifier['tableName'] === 'tt_content' && $this->isActiveOnKey($identifier['dataStructureKey'])) {
+            $content = file_get_contents($this->getPath());
+            if ($content) {
+//                $dataStructure['sheets']['extraEntryEventNews'] = GeneralUtility::xml2array($content);
+		ArrayUtility::mergeRecursiveWithOverrule($dataStructure['sheets'], GeneralUtility::xml2array($content));
+
+            }
+        }
+        return $dataStructure;
+    }
+
+    protected function isActiveOnKey(string $dataStructureKey): bool
+    {
+        $validKeys = ['*,newsstylist_', '*,news_'];
+        $active = false;
+        foreach($validKeys as $prefix) {
+            if (substr($dataStructureKey, 0, strlen($prefix)) === $prefix) {
+                $active = true;
+                break;
+            }
+        }
+        return $active;
+    }
+
+    protected function getPath(): string
+    {
+	$file = (new Typo3Version())->getMajorVersion() >= 12 ? 'newsstylist12.xml' : 'newsstylist.xml';
+        return ExtensionManagementUtility::extPath('newsstylist') . 'Configuration/FlexForms/' . $file;        
+    }
+
+    protected function getPathSwiper(): string
+    {
+	$file = (new Typo3Version())->getMajorVersion() >= 12 ? 'swiperslider12.xml' : 'swiperslider.xml';
+        return ExtensionManagementUtility::extPath('newsstylist') . 'Configuration/FlexForms/' . $file;        
+    }
+
+}
--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "newsstylist".
  *
- * Auto generated 24-07-2022 10:04
+ * Auto generated 24-10-2023 14:59
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array (
   'title' => 'News StyList',
   'description' => 'all in one: one-touch settings within news plugin - fast & cool way to change the output of news, List view 5 layouts (default, card, carousel, newsletter) and swiper slider with some effects',
   'category' => 'plugin',
-  'version' => '2.0.5',
+  'version' => '4.0.1',
   'state' => 'beta',
   'uploadfolder' => false,
   'clearcacheonload' => false,
@@ -25,9 +25,9 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' => 
     array (
-      'typo3' => '11.0.0-11.5.99',
-      'bootstrap_package' => '11.0.3-12.0.99',
-      'news' => '9.0.0-10.99.99',
+      'typo3' => '11.0.0-12.4.99',
+      'bootstrap_package' => '11.0.3-14.0.99',
+      'news' => '9.0.0-11.2.99',
     ),
     'conflicts' => 
     array (
