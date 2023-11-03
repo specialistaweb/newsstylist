34765131c27c1195e8706fb3b84a7c432b9527a1
Alessandro Tuveri <at@specialistaweb.it>
2023-11-03 18:00:01 +0100

    configuration for Hooks
    $GLOBALS[...] Updated

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,28 +4,9 @@
 // all use statements must come first
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
-// Prevent Script from being called directly
-defined('TYPO3') or die();
-
 // encapsulate all locally defined variables
-(function () {
-    // Add your code here
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][\TYPO3\CMS\Core\Configuration\FlexForm\FlexFormTools::class]['flexParsing'][]  = \SpecialistaWeb\Newsstylist\Hooks\newsstylist::class;
-})();
+// Add your code here
 
-/*
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
-
-
-
-// TCEFORM
-//\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/PageTS/TCEFORM.txt">');
-
-
-// add hook for news estension and to load the fields
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][\TYPO3\CMS\Core\Configuration\FlexForm\FlexFormTools::class]['flexParsing'][]  = \SpecialistaWeb\Newsstylist\Hooks\newsstylist::class;
-//$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][\TYPO3\CMS\Core\Configuration\FlexForm\FlexFormTools::class]['flexParsing'][]  = \SpecialistaWeb\Newsstylist\Hooks\swiperslider::class;
-*/
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][\TYPO3\CMS\Core\Configuration\FlexForm\FlexFormTools::class]['flexParsing'][]  = \SpecialistaWeb\Newsstylist\Hooks\NewsSlider12::class;
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][\TYPO3\CMS\Core\Configuration\FlexForm\FlexFormTools::class]['flexParsing'][]  = \SpecialistaWeb\Newsstylist\Hooks\newsslider::class;
 
