3c2a651503c59d4d681705140bb674931aa279ab
Alessandro Tuveri <at@specialistaweb.it>
2023-11-04 17:50:30 +0100

    Update ext_localconf.php
    Class name bug

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,6 +7,6 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 // encapsulate all locally defined variables
 // Add your code here
 
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][\TYPO3\CMS\Core\Configuration\FlexForm\FlexFormTools::class]['flexParsing'][]  = \SpecialistaWeb\Newsstylist\Hooks\NewsSlider12::class;
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][\TYPO3\CMS\Core\Configuration\FlexForm\FlexFormTools::class]['flexParsing'][]  = \SpecialistaWeb\Newsstylist\Hooks\NewsStylist12::class;
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][\TYPO3\CMS\Core\Configuration\FlexForm\FlexFormTools::class]['flexParsing'][]  = \SpecialistaWeb\Newsstylist\Hooks\newsslider::class;
 
