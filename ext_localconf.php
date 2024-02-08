e8663a80852209aa7efa6be3236c4150b855073e
Alessandro Tuveri <at@specialistaweb.it>
2024-02-08 17:58:25 +0100

    Update ext_localconf.php
    int FlexForm parsing

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,12 +1,15 @@
 <?php
 
 
+// Prevent Script from being called directly
+defined('TYPO3') or die();
+
 // all use statements must come first
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+//use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 // encapsulate all locally defined variables
 // Add your code here
 
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][\TYPO3\CMS\Core\Configuration\FlexForm\FlexFormTools::class]['flexParsing'][]  = \SpecialistaWeb\Newsstylist\Hooks\NewsStylist12::class;
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][\TYPO3\CMS\Core\Configuration\FlexForm\FlexFormTools::class]['flexParsing'][]  = \SpecialistaWeb\Newsstylist\Hooks\newsstylist::class;
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][\TYPO3\CMS\Core\Configuration\FlexForm\FlexFormTools::class]['flexParsing'][]  = \SpecialistaWeb\Newsstylist\Hooks\NewsStylist12::class;
 
+// UNUSEFUL:	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][\TYPO3\CMS\Core\Configuration\FlexForm\FlexFormTools::class]['flexParsing'][]  = \SpecialistaWeb\Newsstylist\Hooks\newsstylist::class;
