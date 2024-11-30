b2cf854a4a00c6b730194885fa7329482691d7c1
Alessandro Tuveri <at@specialistaweb.it>
2024-11-30 23:08:22 +0100

    Create sys_template.php

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,8 @@
+<?php
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+defined('TYPO3') or die;
+
+ExtensionManagementUtility::addStaticFile('newsstylist', 'Configuration/TypoScript', 'News Stylist');
+
